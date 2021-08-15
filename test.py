f6dfb46abf0b49f7114d50b4ed7d37d28516ae8e
VyacheslavHalpukov <86649325+VyacheslavHalpukov@users.noreply.github.com>
2021-08-15 11:04:07 +0300

    Test (#2)
    * Add in test.py static method
    
    * Add in test.py static method TODO List ERROR
    
    * New class child

--- test.py
+++ test.py
@@ -1,10 +1,32 @@
 class Test:
     # class test
-    def __init__(self):
+    def __init__(self, track):
         self.test = 'Mouse'
+        self.track = track
 
     def print(self, num:int, eggs: str = 'eggs'):
         return num
 
-tes = Test()
+    @staticmethod
+    def print_simple(a, b):
+        return a + b
+
+    def print_track(self):
+        return self.track
+
+
+
+tes = Test(1)
 print(tes.print(1))
+print(Test.print_simple(1, 200))
+
+class Test1(Test):
+
+    # Next generate class
+    def __init__(self, track):
+        super().__init__(track)
+
+tes1 = Test1(12)
+print(Test1.print_simple(22, 44))
+
+print(tes1.print_track())
