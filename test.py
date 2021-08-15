1dea89768ed99e067c74e979c846f089ae8bef1e
VyacheslavHalpukov <86649325+VyacheslavHalpukov@users.noreply.github.com>
2021-08-15 10:31:16 +0300

    Add in test.py static method

--- test.py
+++ test.py
@@ -6,5 +6,11 @@ class Test:
     def print(self, num:int, eggs: str = 'eggs'):
         return num
 
+    @staticmethod
+    def print_simple(a, b):
+        return a + b
+
+
 tes = Test()
 print(tes.print(1))
+print(Test.print_simple(1, 20))
