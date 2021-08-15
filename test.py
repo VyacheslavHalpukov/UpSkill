1223ce3c6bcac0a90f8cf58abc96036e3ced21c5
VyacheslavHalpukov <86649325+VyacheslavHalpukov@users.noreply.github.com>
2021-08-15 11:01:43 +0300

    New class child

--- test.py
+++ test.py
@@ -1,7 +1,8 @@
 class Test:
     # class test
-    def __init__(self):
+    def __init__(self, track):
         self.test = 'Mouse'
+        self.track = track
 
     def print(self, num:int, eggs: str = 'eggs'):
         return num
@@ -10,7 +11,22 @@ class Test:
     def print_simple(a, b):
         return a + b
 
+    def print_track(self):
+        return self.track
 
-tes = Test()
+
+
+tes = Test(1)
 print(tes.print(1))
 print(Test.print_simple(1, 200))
+
+class Test1(Test):
+
+    # Next generate class
+    def __init__(self, track):
+        super().__init__(track)
+
+tes1 = Test1(12)
+print(Test1.print_simple(22, 44))
+
+print(tes1.print_track())
