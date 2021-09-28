1bfbaf0a6ace85047f52e7050f545ddcfa319f57
VyacheslavHalpukov <86649325+VyacheslavHalpukov@users.noreply.github.com>
2021-09-28 18:45:08 +0300

    child - parents

--- for_up_level/up_1.py
+++ for_up_level/up_1.py
@@ -1,3 +1,21 @@
 for i in range(5):
     print(i)
-else: print('END')
+else:
+    print('END')
+
+class One:
+
+    def sum(self,a,b):
+        return a+b
+
+class Two(One):
+    pass
+
+o = One()
+b= Two()
+
+k = b.sum(2,3)
+
+p = o.sum(1,2)
+
+print(p, k)
