7a7df8ae0a0f7004334a1b305cc71a1b8b1c5ee0
VyacheslavHalpukov <86649325+VyacheslavHalpukov@users.noreply.github.com>
2021-09-29 11:46:32 +0300

    decorator modification add args

--- for_up_level/up_decorator.py
+++ for_up_level/up_decorator.py
@@ -1,14 +1,15 @@
 def a_decorator_passing_arguments(function_to_decorate):
-    def a_wrapper_accepting_arguments(arg1, arg2):
-        print("Смотри, что я получил:", arg1, arg2)
-        function_to_decorate(arg1, arg2)
+    def a_wrapper_accepting_arguments(*args):
+        print("Смотри, что я получил:", *args)
+        function_to_decorate(*args)
+        print("Text after")
     return a_wrapper_accepting_arguments
 
 # Теперь, когда мы вызываем функцию, которую возвращает декоратор, мы вызываем её "обёртку",
 # передаём ей аргументы и уже в свою очередь она передаёт их декорируемой функции
 @a_decorator_passing_arguments
-def print_full_name(first_name, last_name):
-    print("Меня зовут", first_name, last_name)
+def print_full_name(*args):
+    print("Меня зовут", *args)
 
 print_full_name("Vasya", "Pupkin")
 
