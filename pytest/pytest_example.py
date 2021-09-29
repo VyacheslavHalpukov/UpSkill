fe38c1bd5fdb3fa5a52110eee05bed0b57c312e4
VyacheslavHalpukov <86649325+VyacheslavHalpukov@users.noreply.github.com>
2021-09-29 19:22:34 +0300

    Practice (#3)
    * correct some methods after merge
    
    * add and write asserts

---
+++ pytest/pytest_example.py
@@ -0,0 +1,24 @@
+def inc(x):
+    return x + 1
+
+
+def test_inc():
+    assert inc(3) == 4, 'Not Work'
+
+class ClassT:
+
+    def methods(self, y):
+        return y*2
+
+
+print(inc(20))
+
+def test_class():
+    t = ClassT()
+    assert t.methods(2) == 4, 'Not Work'
+    assert t.methods(3) == 6, 'NOT'
+
+def test_class1():
+    t = ClassT()
+    assert t.methods(3) == 6, 'NOT'
+
