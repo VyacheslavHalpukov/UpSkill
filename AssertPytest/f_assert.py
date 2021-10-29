e2a12c353d28023aa33f76585771e2f7dedf93f0
VyacheslavHalpukov <86649325+VyacheslavHalpukov@users.noreply.github.com>
2021-10-29 23:03:03 +0300

    add import and assert

---
+++ AssertPytest/f_assert.py
@@ -0,0 +1,5 @@
+from AssertPytest.p_assert import Frea
+
+def test_f():
+
+    assert Frea().f(1) == 4
---
+++ AssertPytest/p_assert.py
@@ -0,0 +1,9 @@
+class Frea:
+
+    def f(self, a):
+        return 3 + a
+
+
+
+d = Frea()
+print(d.f(1))
