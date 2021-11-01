f9e669b3016d74bd591c29cc24f240824d2670a1
VyacheslavHalpukov <86649325+VyacheslavHalpukov@users.noreply.github.com>
2021-11-01 23:09:33 +0300

    CodeWars find_smallest_int
    start countTheSmileyFaces

---
+++ 6kyu/countTheSmileyFaces.py
@@ -0,0 +1,6 @@
+def count_smileys(arr):
+    lib_smiles = [':D',':~)',';~D',':)']
+    print(arr in lib_smiles)
+    # if arr in
+
+count_smileys(':)')
---
+++ 8kyu/find_smallest_int.py
@@ -0,0 +1,5 @@
+def find_smallest_int(arr):
+    return min(arr)
+
+min = find_smallest_int([78, 56, 232, 12, 11, 43])
+print(min)
