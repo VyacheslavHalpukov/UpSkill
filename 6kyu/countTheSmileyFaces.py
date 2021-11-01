6ca7979946b8cfb86d5fba43c8a49b380cd0eb13
VyacheslavHalpukov <86649325+VyacheslavHalpukov@users.noreply.github.com>
2021-11-01 23:19:34 +0300

    CodeWars countTheSmileyFaces continius

--- 6kyu/countTheSmileyFaces.py
+++ 6kyu/countTheSmileyFaces.py
@@ -1,6 +1,9 @@
 def count_smileys(arr):
+    alone_smiles = arr[1]
+    # TODO Write For where ste[ by step smile
+    print(alone_smiles)
     lib_smiles = [':D',':~)',';~D',':)']
     print(arr in lib_smiles)
     # if arr in
 
-count_smileys(':)')
+count_smileys([':)',';]'])
