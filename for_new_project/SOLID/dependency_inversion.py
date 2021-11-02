254c7aeec579d81e1fd1cbdd05ee45a8aeeeea02
VyacheslavHalpukov <86649325+VyacheslavHalpukov@users.noreply.github.com>
2021-11-02 21:05:01 +0300

    SOLID dependency_inversion principe add more information

--- for_new_project/SOLID/dependency_inversion.py
+++ for_new_project/SOLID/dependency_inversion.py
@@ -16,12 +16,22 @@ class Log:
         notify().print_msg(f'{self.msg} {message}')
         # Enter class in method!!!
 
+    def log_a(self, msg):
+        A().print_msg(f'{self.msg} {msg}')
+
+    def log_b(self, msg):
+        A().print_msg(f'{self.msg} {msg}')
+
 logger = Log()
-logger.log('Test A', A)
-logger.log('Test B', B)
+logger.log('After A', A)
+logger.log('After B', B)
+logger.log_a('Before A')
+logger.log_b('Before B')
 
 """
-Class A: LOGGER Test A
-Class B: LOGGER Test B
+Class A: LOGGER After A
+Class B: LOGGER After B
+Class A: LOGGER Before A
+Class A: LOGGER Before B
 """
 #
