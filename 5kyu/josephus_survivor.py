05309ac51606bb472552f2e9d5791cb8c16f4bde
VyacheslavHalpukov <86649325+VyacheslavHalpukov@users.noreply.github.com>
2021-11-02 18:51:56 +0300

    CodeWars start josephus_survivor problem with recursive continues

--- 5kyu/josephus_survivor.py
+++ 5kyu/josephus_survivor.py
@@ -1,14 +1,29 @@
 def josephus_survivor(n,k):
 
     init_sequance = [i for i in range(1, n + 1)]
-    for i in range(n):
-        pass
-        # TODO Think about logic maybe recursive
 
-    if len(init_sequance) == 1:
-        return init_sequance[0]
-    else:
-        return None
+    def order(line):
+
+        return line
+
+    out = order(init_sequance)
+
+    # def factorial(n):
+    #     if n == 0:
+    #         return 1
+    #     else:
+    #         return n * factorial(n - 1)
+
+    return out[k:] + out[:k-1], out[k-1]
+
+    # for i in range(n):
+    #     pass
+    #     # TODO Think about logic maybe recursive
+    #
+    # if len(init_sequance) == 1:
+    #     return init_sequance[0]
+    # else:
+    #     return None
 
 
 out = josephus_survivor(7,3)
