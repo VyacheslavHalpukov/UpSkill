e1c655111ab92c7787053bfdd3a6f55611c1ea30
VyacheslavHalpukov <86649325+VyacheslavHalpukov@users.noreply.github.com>
2021-11-02 17:21:22 +0300

    CodeWars start josephus_survivor problem by order problem with recursive

--- 5kyu/josephus_survivor.py
+++ 5kyu/josephus_survivor.py
@@ -1,11 +1,15 @@
 def josephus_survivor(n,k):
-    x = 0
-    init_sequance = [i for i in range(1, n+1)]
-    while x < 15:
-        z = init_sequance.pop(3)
-        print(x, z)
-        x += 1
-    return init_sequance
+
+    init_sequance = [i for i in range(1, n + 1)]
+    for i in range(n):
+        pass
+        # TODO Think about logic maybe recursive
+
+    if len(init_sequance) == 1:
+        return init_sequance[0]
+    else:
+        return None
+
 
 out = josephus_survivor(7,3)
 print(out)
