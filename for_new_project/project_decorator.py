6a33e0ab060ede5a0adef88a78d67db375307944
VyacheslavHalpukov <86649325+VyacheslavHalpukov@users.noreply.github.com>
2021-11-03 12:56:18 +0300

    correct decorator arguments

--- for_new_project/project_decorator.py
+++ for_new_project/project_decorator.py
@@ -1,3 +1,4 @@
+
 def decoratora(foo):
     def wraper(*args):
         print('Before')
@@ -5,7 +6,18 @@ def decoratora(foo):
         print('After')
     return wraper
 
-@decoratora
+def psevdo(decor_text):
+    def decoratora(foo):
+        def wraper(*args):
+            print('Before')
+            # print((decor_text))
+            foo(decor_text)
+            foo(*args)
+            print('After')
+        return wraper
+    return decoratora
+
+@psevdo('Hi')
 def hart(text):
     print(text)
 
