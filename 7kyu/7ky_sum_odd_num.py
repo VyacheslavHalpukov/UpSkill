2fce7b3d63aafbdd2a4ee0b40150bcc1a9e178ee
VyacheslavHalpukov <86649325+VyacheslavHalpukov@users.noreply.github.com>
2021-11-19 19:57:15 +0300

    7ky_sum_odd_num.py

---
+++ 7kyu/7ky_sum_odd_num.py
@@ -0,0 +1,19 @@
+class Triangle:
+
+    def __init__(self, number):
+        self.number = number
+
+    def sum_triangle(self):
+        triangle = [i for i in range(self.number ** 2 + self.number) if i % 2 != 0]
+        return sum(triangle[len(triangle) - self.number:])
+
+def row_sum_odd_numbers(n):
+
+    triangle = Triangle(n)
+    return triangle.sum_triangle()
+
+
+num = row_sum_odd_numbers(3)
+print(num)
+
+
