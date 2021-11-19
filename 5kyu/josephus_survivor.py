6348095b001cd2cdd33f80862f3feaced4e4d025
VyacheslavHalpukov <86649325+VyacheslavHalpukov@users.noreply.github.com>
2021-11-19 21:04:37 +0300

    finished josephus_survivor.py

--- 5kyu/josephus_survivor.py
+++ 5kyu/josephus_survivor.py
@@ -1,30 +1,48 @@
-def josephus_survivor(n,k):
+# def josephus_survivor(n,k):
+#
+#     init_sequance = [i for i in range(1, n + 1)]
+# new_arr = []
 
-    init_sequance = [i for i in range(1, n + 1)]
-
-    def order(line):
-
-        return line
-
-    out = order(init_sequance)
-
-    # def factorial(n):
-    #     if n == 0:
-    #         return 1
-    #     else:
-    #         return n * factorial(n - 1)
-
-    return out[k:] + out[:k-1], out[k-1]
-
-    # for i in range(n):
-    #     pass
-    #     # TODO Think about logic maybe recursive
-    #
-    # if len(init_sequance) == 1:
-    #     return init_sequance[0]
-    # else:
-    #     return None
+def josephus(n, k):
+    new_arr = []
 
 
-out = josephus_survivor(7,3)
+    def insert_josephus(n,k):
+
+        if k == 1:
+            return n
+        elif len(n) > k:
+            prom = n[k:] + n[:k - 1]
+            # print(prom, n[k-1])
+            new_arr.append(n[k-1])
+            return (insert_josephus(prom, k))
+        elif len(n) == k:
+            prom = n[:-1]
+            # print(prom, n[k-1])
+            new_arr.append(n[k - 1])
+            return (insert_josephus(prom, k))
+        elif len(n) < k and len(n) > 1:
+            new_k = k - len(n)
+            prom = n[new_k:] + n[:new_k - 1]
+            # print(prom, n[new_k-1])
+            new_arr.append(n[new_k - 1])
+            return (insert_josephus(prom, k))
+        else:
+            # print(n)
+            new_arr.append(n[0])
+            return new_arr
+    if n == []:
+        return n
+    else:
+        out = insert_josephus(n, k)
+        return out
+
+# return inita(init_sequance, k)
+
+
+
+out = josephus(["C","o","d","e","W","a","r","s"],4)
 print(out)
+
+# f = inita([1,2,3,4,5,6,7,8,9,10,11], 3)
+# print(f)
