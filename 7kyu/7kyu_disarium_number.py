b1ca1c88a107671d9f6953976ec11b0e8eec764e
v.halpukov <vhalpukov@gmail.com>
2021-11-26 21:44:19 +0300

    correct 7kyu_disarium_number.py add functionality change to correct variant

--- 7kyu/7kyu_disarium_number.py
+++ 7kyu/7kyu_disarium_number.py
@@ -1,10 +1,14 @@
 def disarium_number(number):
-    new_number = list(number)
+    number_str = str(number)
+    new_number = list(number_str)
     result_number = 0
     for i, j in enumerate(new_number):
-        result_number = result_number + int(j)**int(i)
-    return result_number
+        result_number = result_number + int(j)**int(i + 1)
+    if number == result_number:
+        return "Disarium !!"
+    else:
+        return "Not !!"
     # return new_number
 
-nu = disarium_number('12')
+nu = disarium_number(1024)
 print(nu)
