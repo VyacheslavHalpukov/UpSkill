f6af7a99bbff7a703d1ae6a131b3d53e01e9c05f
v.halpukov <vhalpukov@gmail.com>
2021-11-26 16:28:36 +0300

    7kyu_disarium_number.py add functionality change to correct variant

--- 7kyu/7kyu_disarium_number.py
+++ 7kyu/7kyu_disarium_number.py
@@ -1,6 +1,10 @@
 def disarium_number(number):
-    new_number = number.join('1')
-    return new_number
+    new_number = list(number)
+    result_number = 0
+    for i, j in enumerate(new_number):
+        result_number = result_number + int(j)**int(i)
+    return result_number
+    # return new_number
 
 nu = disarium_number('12')
 print(nu)
