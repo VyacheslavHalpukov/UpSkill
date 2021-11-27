546713418da8f80800d51de6d3aac0d3173a17b8
v.halpukov <vhalpukov@gmail.com>
2021-11-27 12:32:10 +0300

    6kyu_tank_truck.py add in task add class Circle square add alfa in degrees and radians

--- 6kyu/6kyu_tank_truck.py
+++ 6kyu/6kyu_tank_truck.py
@@ -1,3 +1,22 @@
+import math
+
+class CircleSquare:
+
+    def __init__(self, h, d):
+        self.h = h
+        self.d = d
+
+    def alfa(self):
+        radius = self.d/2
+        alfa = 2*math.acos(1-self.h/radius)
+        return math.degrees(alfa), alfa
+
+
+    def square_segment(self):
+
+
+        pass
+
 def tankvol(h, d, vt):
 
     pass
@@ -5,3 +24,6 @@ def tankvol(h, d, vt):
 # Pi * r**2 * h
 # This form volume
 # all screnshots in folder
+circle = CircleSquare(12, 1500)
+alfa_deg, alfa_rad = circle.alfa()
+print(alfa_deg)
