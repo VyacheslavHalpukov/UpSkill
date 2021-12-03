e7d45a1c9ef85fdb7adeed647564d64abd015979
VyacheslavHalpukov <86649325+VyacheslavHalpukov@users.noreply.github.com>
2021-12-03 19:21:27 +0300

    write class hash

--- hash_file/hash_file.py
+++ hash_file/hash_file.py
@@ -1,2 +1,23 @@
+import hashlib
+
 class Hash:
-    pass
+
+    def __init__(self, string_a=None):
+        self.string = string_a
+
+    def print_hw(self):
+        id_l = hashlib.sha256(b'Hello world').hexdigest()
+        return id_l, len(id_l)
+
+    def get_hash(self):
+        str_to_bytes = bytes(self.string, 'utf-8')
+        hasg_object = hashlib.sha256(str_to_bytes)
+        return hasg_object.hexdigest()
+
+obj = Hash('Hd')
+fr_om = obj.get_hash()
+ideal, len = obj.print_hw()
+
+print(fr_om)
+print(ideal, len)
+
